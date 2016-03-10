611c65e40fef713bb8f37128a14931ee3539ec5a
한솔 정 <hansoli@live.co.kr>
2016-03-10 23:46:12 +0900

    My Sukje

---
+++ algorithms1/Day1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace algorithms1 {
+    class Day1 {
+        #region Class1
+        public int Class1(int n) {
+            // base
+            if (n % 2 == 0 || n < 1) return 0;
+            // recursion
+            return Class1(n - 2) + Class1(n - 1) + n;
+        }
+        #endregion
+        #region Class2
+        public int Class2(int n, int k) {
+            // base
+            if (k == 0) return 1;
+            if (k == 1) return n;
+            if (k > n) return 0;
+            // recursion
+            return Class2(n - 1, k) + Class2(n - 1, k - 1);
+        }
+        #endregion
+        #region Class4
+        public int Class4(int n) {
+            // base
+            if (n <= 1) return 1;
+            // recursion
+            return Class4(n - 1) + Class4(n - 2);
+        }
+        #endregion
+        #region Class5
+        public int Class5(int n) {
+            // base
+            if (n == 1) return 1;
+            // recursion
+            return n * (n + 1) / 2 + Class5(n - 1);
+        }
+        #endregion
+        #region Class6
+        public bool Class6(string s) {
+            // base
+            if (s.Length <= 1) return true;
+            // recursion
+            return (s[0] == s[s.Length - 1]) && Class6(s.Substring(1, s.Length - 2));
+        }
+        #endregion
+        #region Class7
+        public int Class7_Compare(string s1, string s2) {
+            if (s1.Length > 0 && s2.Length == 0) return 1;
+            else if (s1.Length == 0 && s2.Length > 0) return -1;
+            else if (s1.Length == 0 && s2.Length == 0) return 0;
+
+            if (s1[0] < s2[0]) return -1;
+            else if (s1[0] > s2[0]) return 1;
+            else return Class7_Compare(s1.Substring(1), s2.Substring(1));
+        }
+        #endregion
+        #region Class8
+        public int Class8(IList<int> data, int left, int right, int sum) {
+            if (left == right) return 0;
+            return Class8(data, left + 1, right, sum) + Class8_Right(data, left, right, sum);
+        }
+        public int Class8_Right(IList<int> data, int left, int right, int sum) {
+            if (left == right) return 0;
+            if (data[left] + data[right] < sum) return 0;
+            if (data[left] + data[right] == sum) return 1; // 검색 성공
+            else return Class8_Right(data, left, right - 1, sum);
+        }
+        #endregion
+    }
+}
--- algorithms1/Program.cs
+++ algorithms1/Program.cs
@@ -1,40 +1,69 @@
-﻿#define CLASS6
+﻿#define Day1
+#define Class8
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace algorithms1 {
     class Program {
         static void Main(string[] args) {
-            Class r = new Class();
-#if CLASS1
+#if Day1
+            Day1 r = new Day1();
+#if Class1
             Console.Write("Input the number (Class1): ");
-            string s = Console.ReadLine();
-            Console.WriteLine($"Result is {r.Class1(int.Parse(s))}");
+            int s = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Result is {r.Class1(s)}");
 #endif
-#if CLASS2
+#if Class2
             Console.Write("Input the number 1 (Class2): ");
-            string s1 = Console.ReadLine();
+            int s1 = int.Parse(Console.ReadLine());
             Console.Write("Input the number 2 (Class2): ");
-            string s2 = Console.ReadLine();
-            Console.WriteLine($"Result is {r.Class2(int.Parse(s1), int.Parse(s2))}");
+            int s2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Result is {r.Class2(s1, s2)}");
 #endif
-#if CLASS4
+#if Class4
             Console.Write("Input the number (Class4): ");
-            string s = Console.ReadLine();
+            int s = int.Parse(Console.ReadLine());
             // 피보나치 수열임
             // 순환식: R_n = R_(n-1) + R_(n-2) , R_z | z=0,1 = 1
-            Console.WriteLine($"Result is {r.Class4(int.Parse(s))}");
+            Console.WriteLine($"Result is {r.Class4(s)}");
 #endif
-#if CLASS5
+#if Class5
             Console.Write("Input the number (Class5): ");
-            string s = Console.ReadLine();
+            int s = int.Parse(Console.ReadLine());
             // n번째 층의 공의 갯수는 1부터 시작하고 마지막 항이 n인 등차수열의 합
             // 순환식: R_n = n*(n+1)/2 + R_(n-1) , R_1 = 1
-            Console.WriteLine($"Result is {r.Class5(int.Parse(s))}");
+            Console.WriteLine($"Result is {r.Class5(s)}");
 #endif
-#if CLASS6
-            Console.Write("Input the number (Class6): ");
-
+#if Class6
+            Console.Write("Input the string (Class6): ");
+            string s = Console.ReadLine();
+            if (r.Class6(s)) Console.WriteLine("It is palindrome");
+            else Console.WriteLine("It is not palindrome");
+#endif
+#if Class7
+            Console.Write("Input the string1 (Class7): ");
+            string s1 = Console.ReadLine();
+            Console.Write("Input the string2 (Class7): ");
+            string s2 = Console.ReadLine();
+            int res = r.Class7_Compare(s1, s2);
+            if (res < 0) {
+                Console.WriteLine("String 1 is first, String 2 is second.");
+            } else if (res == 0) {
+                Console.WriteLine("String 1 and 2 are same.");
+            } else {
+                Console.WriteLine("String 1 is second, String 2 is first.");
+            }
+#endif
+#if Class8
+            Console.Write("Input the white-space seperated numbers (Class8): ");
+            string s1 = Console.ReadLine();
+            IList<int> s2 = s1.Split(' ').Select(x => int.Parse(x)).OrderBy(x => x).ToList();
+            Console.Write("Input the sum number (Class8): ");
+            int s3 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Result is {r.Class8(s2, 0, s2.Count - 1, s3)}");
+#endif
 #endif
             Console.ReadLine(); // 자동 종료 방지
         }
