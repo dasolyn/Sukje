4a82239b1afa9ea4cd1031ed29ab7e4b0ff0dade
한솔 정 <hansoli@live.co.kr>
2016-03-10 22:29:59 +0900

    Initial Commit

---
+++ algorithms1/Class.cs
@@ -0,0 +1,38 @@
+﻿namespace algorithms1 {
+    class Class {
+        #region Class1
+        public int Class1(int n) {
+            // base
+            if (n % 2 == 0 || n < 1) return 0;
+            // recursion
+            return Class1(n - 2) + Class1(n - 1) + n;
+        }
+        #endregion
+        #region Class2
+        public int Class2(int n, int k) {
+            // base
+            if (k == 0) return 1;
+            if (k == 1) return n;
+            if (k > n) return 0;
+            // recursion
+            return Class2(n - 1, k) + Class2(n - 1, k - 1);
+        }
+        #endregion
+        #region Class4
+        public int Class4(int n) {
+            // base
+            if (n <= 1) return 1;
+            // recursion
+            return Class4(n - 1) + Class4(n - 2);
+        }
+        #endregion
+        #region Class5
+        public int Class5(int n) {
+            // base
+            if (n == 1) return 1;
+            // recursion
+            return n * (n + 1) / 2 + Class5(n - 1);
+        }
+        #endregion
+    }
+}
---
+++ algorithms1/Program.cs
@@ -0,0 +1,42 @@
+﻿#define CLASS6
+
+using System;
+
+namespace algorithms1 {
+    class Program {
+        static void Main(string[] args) {
+            Class r = new Class();
+#if CLASS1
+            Console.Write("Input the number (Class1): ");
+            string s = Console.ReadLine();
+            Console.WriteLine($"Result is {r.Class1(int.Parse(s))}");
+#endif
+#if CLASS2
+            Console.Write("Input the number 1 (Class2): ");
+            string s1 = Console.ReadLine();
+            Console.Write("Input the number 2 (Class2): ");
+            string s2 = Console.ReadLine();
+            Console.WriteLine($"Result is {r.Class2(int.Parse(s1), int.Parse(s2))}");
+#endif
+#if CLASS4
+            Console.Write("Input the number (Class4): ");
+            string s = Console.ReadLine();
+            // 피보나치 수열임
+            // 순환식: R_n = R_(n-1) + R_(n-2) , R_z | z=0,1 = 1
+            Console.WriteLine($"Result is {r.Class4(int.Parse(s))}");
+#endif
+#if CLASS5
+            Console.Write("Input the number (Class5): ");
+            string s = Console.ReadLine();
+            // n번째 층의 공의 갯수는 1부터 시작하고 마지막 항이 n인 등차수열의 합
+            // 순환식: R_n = n*(n+1)/2 + R_(n-1) , R_1 = 1
+            Console.WriteLine($"Result is {r.Class5(int.Parse(s))}");
+#endif
+#if CLASS6
+            Console.Write("Input the number (Class6): ");
+
+#endif
+            Console.ReadLine(); // 자동 종료 방지
+        }
+    }
+}
