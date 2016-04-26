ae7bb05224c23429560468262de965374f87dfd1
정한솔 <hansoli@live.co.kr>
2016-04-26 14:53:35 +0900

    해시셋

---
+++ HashSet/MyHashSet.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HashSet {
+    class MyHashSet<T> : ICollection<T> {
+        public class HashNode<TInternalData> {
+            public HashNode<TInternalData> Next;
+            public int HashValue {
+                get {
+                    return Math.Abs(Data.GetHashCode());
+                }
+            }
+            public TInternalData Data;
+            public HashNode(TInternalData Data) {
+                this.Data = Data;
+            }
+        }
+        public HashNode<T>[] Source;
+        private float LoadFactor;
+        private void ResizeIfBig() {
+            if (Count > LoadFactor * Source.Length) {
+                HashNode<T>[] newarray = new HashNode<T>[Source.Length * 2];
+                int cursorindex = 0;
+                HashNode<T> cursor = Source[0];
+                while (true) {
+                    if (cursor == null) {
+                        cursorindex++;
+                        if (cursorindex >= Source.Length) break;
+                        cursor = Source[cursorindex];
+                    } else {
+                        newarray[cursor.HashValue % newarray.Length] = cursor;
+                        if (cursor.Next == null) {
+                            cursorindex++;
+                            if (cursorindex >= Source.Length) break;
+                            cursor = Source[cursorindex];
+                        } else cursor = cursor.Next;
+                    }
+                }
+                Source = newarray;
+            }
+        }
+        public int Count { get; private set; } = 0;
+        public bool IsReadOnly { get; } = false;
+        public MyHashSet() : this(4, 0.8f) { }
+        public MyHashSet(int Capacity, float LoadFactor) {
+            Source = new HashNode<T>[Capacity];
+            this.LoadFactor = LoadFactor;
+        }
+        public bool Add(T item) {
+            int hashindex = Math.Abs(item.GetHashCode() % Source.Length);
+            if (Source[hashindex] == null) {
+                Source[hashindex] = new HashNode<T>(item);
+            } else {
+                HashNode<T> cursor = Source[hashindex];
+                while (true) {
+                    if (cursor.Data.Equals(item)) return false;
+                    if (cursor.Next == null) break;
+                    else cursor = cursor.Next;
+                }
+                cursor.Next = new HashNode<T>(item);
+            }
+            Count++;
+            ResizeIfBig();
+            return true;
+        }
+        public void Clear() {
+            for (int i = 0; i < Source.Length; i++) Source[i] = null;
+        }
+        public bool Contains(T item) {
+            int hashindex = Math.Abs(item.GetHashCode() % Source.Length);
+            if (Source[hashindex] == null) return false;
+            else {
+                HashNode<T> cursor = Source[hashindex];
+                while (true) {
+                    if (cursor.Data.Equals(item)) return true;
+                    if (cursor.Next == null) return false;
+                    else cursor = cursor.Next;
+                }
+            }
+        }
+        public void CopyTo(T[] array, int arrayIndex) {
+            IEnumerator<T> etor = GetEnumerator();
+            for (int i = arrayIndex; i < array.Length; i++) {
+                if (!etor.MoveNext()) break;
+                array[i] = etor.Current;
+            }
+            etor.Dispose();
+        }
+        public IEnumerator<T> GetEnumerator() {
+            int cursorindex = 0;
+            HashNode<T> cursor = Source[0];
+            while (true) {
+                if (cursor == null) {
+                    cursorindex++;
+                    if (cursorindex >= Source.Length) yield break;
+                    else cursor = Source[cursorindex];
+                } else {
+                    yield return cursor.Data;
+                    if (cursor.Next == null) {
+                        cursorindex++;
+                        if (cursorindex >= Source.Length) yield break;
+                        else cursor = Source[cursorindex];
+                    } else cursor = cursor.Next;
+                }
+            }
+        }
+        public bool Remove(T item) {
+            int hashindex = Math.Abs(item.GetHashCode() % Source.Length);
+            if (Source[hashindex] == null) return false;
+            else {
+                HashNode<T> parent = null;
+                HashNode<T> cursor = Source[hashindex];
+                while (true) {
+                    if (cursor.Data.Equals(item)) {
+                        if (parent == null) Source[hashindex] = cursor.Next;
+                        else parent.Next = cursor.Next;
+                        Count--;
+                        return true;
+                    }
+                    if (cursor.Next == null) {
+                        return false;
+                    } else {
+                        parent = cursor;
+                        cursor = cursor.Next;
+                    }
+                }
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator() {
+            foreach (var i in this) yield return i;
+        }
+        public bool IsEmpty() {
+            return Count == 0;
+        }
+        void ICollection<T>.Add(T item) {
+            Add(item);
+        }
+    }
+}
---
+++ HashSet/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace HashSet {
+    class Program {
+        static void Main(string[] args) {
+            MyHashSet<string> myhashset = new MyHashSet<string>();
+            while (true) {
+                Console.Write("$ ");
+                string cmd = Console.ReadLine();
+                string[] splitcmd = cmd.Split(' ');
+                try {
+                    if (splitcmd[0].ToLower() == "add") {
+                        string item = cmd.Replace("add ", "");
+                        if (item.Length > 0) {
+                            if (myhashset.Add(item)) Console.WriteLine("Success");
+                            else Console.WriteLine("Failed : There are duplicate conflict");
+                        } else {
+                            Console.WriteLine("Failed : There are no string to add");
+                        }
+                    } else if (splitcmd[0].ToLower() == "list") {
+                        if (splitcmd.Length == 1) {
+                            int count = 0;
+                            foreach (string i in myhashset) {
+                                Console.Write($"{i} ");
+                                if (++count % 10 == 0) Console.WriteLine();
+                            }
+                            Console.WriteLine();
+                        } else {
+                            if (splitcmd[1].ToLower() == "detail") {
+                                for (int i = 0; i < myhashset.Source.Length; i++) {
+                                    Console.Write($"Hash value {i}: ");
+                                    var cursor = myhashset.Source[i];
+                                    while (cursor != null) {
+                                        Console.Write($"{cursor.Data} ");
+                                        cursor = cursor.Next;
+                                    }
+                                    Console.WriteLine();
+                                }
+                            }
+                        }
+                    } else if (splitcmd[0].ToLower() == "remove") {
+                        string item = cmd.Replace("remove", "");
+                        if (item.Length > 0) {
+                            if (myhashset.Remove(item)) Console.WriteLine("Success");
+                            else Console.WriteLine("Failed : There are no specific element");
+                        } else {
+                            Console.WriteLine("Failed : There are no string to delete");
+                        }
+                    } else if (splitcmd[0].ToLower() == "find") {
+                        string item = cmd.Replace("find", "");
+                        if (item.Length > 0) {
+                            if (myhashset.Contains(item)) Console.WriteLine("Success : Specific element is in HashSet");
+                            else Console.WriteLine("Failed : There are no specific element");
+                        } else {
+                            Console.WriteLine("Failed : There are no string to delete");
+                        }
+                    } else {
+                        Console.WriteLine("command: add (item), list, remove, find");
+                    }
+                } catch (ArgumentException) {
+                    Console.WriteLine("command: add (item), list, remove, find");
+                }
+            }
+        }
+    }
+}
