4794d169fcd5ee9d8f36e036269b01413781490d
한솔 정 <hansoli@live.co.kr>
2016-05-11 20:59:07 +0900

    1번문제 끝

--- Graph/Graph/MyGraph.cs
+++ Graph/Graph/MyGraph.cs
@@ -43,25 +43,43 @@ namespace Graph {
                 }
             }
         }
-        public void MakeNewEdge(int a, int b) {
-            if (source[a] == null || source[b] == null) throw new ArgumentException();
-            if (a == b) throw new ArgumentException();
-            if (CheckEdge(a, b)) throw new ArgumentException();
-            Node<T> temp = source[a];
-            while (temp.Next != null) temp = temp.Next;
-            temp.Next = new Node<T> { Index = b };
-            temp = source[b];
-            while (temp.Next != null) temp = temp.Next;
-            temp.Next = new Node<T> { Index = a };
+        public IEnumerable<T> AsTopologyOrderEnumerable() {
+            int[] visited = new int[Size];
+            visited.Initialize();
+            LinkedList<T> r = new LinkedList<T>();
+            for (int i = 0; i < Size; i++)
+                if (visited[i] == 0) InternalDFSTraversal(i, visited, r);
+            foreach (T i in r) yield return i;
         }
-        public bool CheckEdge(int a, int b) {
-            if (a == b) return false;
-            Node<T> temp = source[a];
+        private void InternalDFSTraversal(int index, int[] visited, LinkedList<T> linkedlist) {
+            visited[index] = 1;
+            Node<T> temp = source[index];
             while (temp.Next != null) {
-                if (temp.Index == b) return true;
+                if (visited[temp.Next.Index] == 0) InternalDFSTraversal(temp.Next.Index, visited, linkedlist);
+                temp = temp.Next;
+            }
+            linkedlist.AddFirst(source[index].Data);
+        }
+        public void MakeEdge(int a, int b) {
+            MakeDirectedEdge(a, b);
+            MakeDirectedEdge(b, a);
+        }
+        public bool CheckEdge(int from, int to) {
+            if (from == to) return false;
+            Node<T> temp = source[from];
+            while (temp.Next != null) {
+                if (temp.Index == to) return true;
                 temp = temp.Next;
             }
             return false;
+        }
+        public void MakeDirectedEdge(int from, int to) {
+            if (source[from] == null || source[to] == null) throw new ArgumentException();
+            if (from == to) throw new ArgumentException();
+            if (CheckEdge(from, to)) throw new ArgumentException();
+            Node<T> temp = source[from];
+            while (temp.Next != null) temp = temp.Next;
+            temp.Next = new Node<T> { Index = to };
         }
         private class Node<TNode> {
             public int Index = -1;
