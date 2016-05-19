65bd4161ef0b943b4609d5b2f03b5e2bbb63b876
한솔 정 <hansoli@live.co.kr>
2016-05-19 22:07:33 +0900

    다익스트라 그래프로 분리

---
+++ ShortestPath/DijkstraGraph.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShortestPath {
+    /// <summary>
+    /// 다익스트라 알고리즘을 사용하여 두 점 사이의 최단 경로를 구할 수 있는 무방향 그래프입니다.
+    /// </summary>
+    class DijkstraGraph<TKey, TValue> where TKey : IComparable<TKey>, IComparable, IEquatable<TKey> {
+        private SortedDictionary<TKey, Node<TValue>> nodes = new SortedDictionary<TKey, Node<TValue>>();
+        /// <summary>
+        /// 그래프에 포함된 점의 개수입니다.
+        /// </summary>
+        public int Size {
+            get {
+                return nodes.Count;
+            }
+        }
+        public TValue this[TKey key] {
+            get {
+                return nodes[key].Data;
+            }
+            set {
+                nodes[key] = new Node<TValue>(value);
+            }
+        }
+        /// <summary>
+        /// 해당 두 점을 잇는 변의 가중치를 반환합니다.
+        /// </summary>
+        /// <exception cref="ArgumentException">해당 두 점을 잇는 변이 존재하지 않습니다.</exception>
+        public double GetWeightOfEdge(TKey from, TKey to) {
+            if (nodes.ContainsKey(from) && nodes.ContainsKey(to) && nodes[from].Sibling.ContainsKey(to)) return nodes[from].Sibling[to];
+            else throw new ArgumentException();
+        }
+        /// <summary>
+        /// 해당 두 점을 잇고 지정한 가중치를 갖는 변을 생성합니다.
+        /// </summary>
+        public void MakeEdge(TKey from, TKey to, double weight) {
+            if (nodes.ContainsKey(from) && nodes.ContainsKey(to) && !from.Equals(to) && !CheckEdge(from, to)) {
+                nodes[from].Sibling.Add(to, weight);
+                nodes[to].Sibling.Add(from, weight);
+            } else {
+                throw new ArgumentException();
+            }
+        }
+        /// <summary>
+        /// 해당 두 점을 잇는 변이 존재하는지 검사합니다.
+        /// </summary>
+        public bool CheckEdge(TKey from, TKey to) {
+            if (nodes.ContainsKey(from) && nodes.ContainsKey(to)) return nodes[from].Sibling.ContainsKey(to);
+            else return false;
+        }
+        /// <summary>
+        /// 해당 두 점을 잇는 가장 가까운 경로를 찾아 반환합니다.
+        /// </summary>
+        public PathResult ShortestPath(TKey from, TKey to) {
+            SortedDictionary<TKey, double> dist = new SortedDictionary<TKey, double>();
+            SortedDictionary<TKey, TKey> prev = new SortedDictionary<TKey, TKey>();
+            MinPQueue<HeapElement> minqueue = new MinPQueue<HeapElement>();
+            SortedSet<TKey> visited = new SortedSet<TKey>();
+            dist.Add(from, 0.0);
+            minqueue.Insert(new HeapElement { Index = from, Distance = dist[from] });
+            bool success = false;
+            while (true) {
+                // 최소값 찾기
+                HeapElement min;
+                try {
+                    do {
+                        min = minqueue.ExtractMin();
+                    } while (visited.Contains(min.Index));
+                } catch (InvalidOperationException) {
+                    break;
+                }
+                // 방문한 것으로 마킹
+                visited.Add(min.Index);
+                if (min.Index.Equals(to)) {
+                    success = true;
+                    break;
+                }
+                // 인접한 에지 relax
+                foreach (var i in nodes[min.Index].Sibling) {
+                    if (!dist.ContainsKey(i.Key) || dist[i.Key] > dist[min.Index] + i.Value) {
+                        dist[i.Key] = dist[min.Index] + i.Value;
+                        minqueue.Insert(new HeapElement { Index = i.Key, Distance = dist[i.Key] });
+                        prev[i.Key] = min.Index;
+                    }
+                }
+            }
+            if (success) {
+                Stack<TKey> result = new Stack<TKey>();
+                TKey cur = to;
+                while (true) {
+                    result.Push(cur);
+                    if (cur.Equals(from)) break;
+                    cur = prev[cur];
+                }
+                return new PathResult { TotalDistance = dist[to], Route = result };
+            } else {
+                return new PathResult { TotalDistance = double.MaxValue, Route = null };
+            }
+        }
+
+        public class PathResult {
+            public double TotalDistance;
+            public Stack<TKey> Route;
+        }
+        private class Node<TNodeValue> {
+            public SortedDictionary<TKey, double> Sibling;
+            public TNodeValue Data;
+            public Node(TNodeValue Data) {
+                this.Data = Data;
+                Sibling = new SortedDictionary<TKey, double>();
+            }
+        }
+        private struct HeapElement : IComparable<HeapElement>, IComparable, IEquatable<HeapElement> {
+            public TKey Index;
+            public double Distance;
+            public int CompareTo(object obj) {
+                HeapElement? ele = obj as HeapElement?;
+                if (!ele.HasValue) throw new ArgumentException();
+                else return CompareTo(ele.Value);
+            }
+            public int CompareTo(HeapElement other) {
+                return Distance.CompareTo(other.Distance);
+            }
+            public bool Equals(HeapElement other) {
+                return Distance.Equals(other.Distance);
+            }
+        }
+    }
+}
--- ShortestPath/Program.cs
+++ ShortestPath/Program.cs
@@ -1,5 +1,4 @@
-﻿using Graph;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -10,9 +9,24 @@ namespace ShortestPath {
             IEnumerable<string> file = File.ReadLines("road_segments.txt").Where(s => s.Trim().Length != 0 && s[0] != '#');
             // 도시 로딩
             int count = int.Parse(file.First());
-            List<string> cities = file.Skip(1).Take(count).ToList();
-            MyGraph<int> graph = new MyGraph<int>();
-            for (int i = 0; i < cities.Count; i++) graph[i] = i;
+            SortedDictionary<string, int> cities = new SortedDictionary<string, int>();
+            DijkstraGraph<int, string> graph = new DijkstraGraph<int, string>();
+            int index = 0;
+            foreach (var i in file.Skip(1).Take(count)) {
+                string dup = i;
+                int avoiddup = 1;
+                while (true) {
+                    try {
+                        cities.Add(dup, index);
+                        break;
+                    } catch (ArgumentException) {
+                        dup = $"{i}_{avoiddup}";
+                        avoiddup++;
+                    }
+                }
+                graph[index] = dup;
+                index++;
+            }
             Console.WriteLine($"도시 로딩이 완료되었습니다. {cities.Count}개의 도시가 있습니다.");
             // 도로 로딩
             file = file.Skip(1 + count);
@@ -21,33 +35,43 @@ namespace ShortestPath {
                 string[] line = i.Split(' ');
                 if (line.Length == 3) graph.MakeEdge(int.Parse(line[0]), int.Parse(line[1]), double.Parse(line[2]));
             }
-            Console.WriteLine($"도로 로딩이 완료되었습니다. {graph.EdgeCount}개의 도로가 있습니다.");
+            Console.WriteLine($"도로 로딩이 완료되었습니다. {count}개의 도로가 있습니다.");
+            Console.WriteLine("중복되는 이름이 있을 경우 2번째 도시부터는 차례대로 _1, _2 ... 를 붙여 구별합니다.");
+            Console.WriteLine("원하는 도시를 찾을 수 없는 경우 중복이 있는지 확인하고 도시 번호로도 찾아보세요.");
             while (true) {
+                Console.WriteLine();
                 Console.Write($"시작점의 이름 또는 번호를 입력하세요. ");
                 string input = Console.ReadLine();
                 int from;
-                if (!int.TryParse(input, out from)) from = cities.FindIndex(s => s == input);
-                if (from < 0 || from >= cities.Count) {
-                    Console.WriteLine("그런 도시가 없습니다. 다시 입력하세요");
-                    continue;
+                try {
+                    from = cities[input];
+                } catch (KeyNotFoundException) {
+                    if (!int.TryParse(input, out from)) {
+                        Console.WriteLine("그런 도시가 없습니다. 다시 입력하세요");
+                        continue;
+                    }
                 }
-                Console.WriteLine($"시작점: {from} ({cities[from]})");
+                Console.WriteLine($"시작점: {from} ({graph[from]})");
                 Console.Write("도착점의 이름 또는 번호를 입력하세요. ");
                 input = Console.ReadLine();
                 int to;
-                if (!int.TryParse(input, out to)) to = cities.FindIndex(s => s == input);
-                if (to < 0 || to >= cities.Count) {
-                    Console.WriteLine("그런 도시가 없습니다. 다시 입력하세요");
-                    continue;
+                try {
+                    to = cities[input];
+                } catch (KeyNotFoundException) {
+                    if (!int.TryParse(input, out to)) {
+                        Console.WriteLine("그런 도시가 없습니다. 다시 입력하세요");
+                        continue;
+                    }
                 }
-                Console.WriteLine($"도착점: {to} ({cities[to]})");
-                var res = graph.DijkstraShortestPath(from, to);
+                Console.WriteLine($"도착점: {to} ({graph[to]})");
+                Console.WriteLine();
+                var res = graph.ShortestPath(from, to);
                 if (res.Route != null) {
                     Console.WriteLine($"최단 거리는 {res.TotalDistance:0.##}입니다.");
                     Console.WriteLine("이하 경로를 출력합니다: ");
                     int prev = res.Route.First();
                     foreach (int i in res.Route.Skip(1)) {
-                        Console.WriteLine($"{prev} ({cities[prev]}) -> {i} ({cities[i]}): Distance is {graph.GetWeightOfEdge(prev, i):0.##}");
+                        Console.WriteLine($"{prev} ({graph[prev]}) -> {i} ({graph[i]}): 거리는 {graph.GetWeightOfEdge(prev, i):0.##}");
                         prev = i;
                     }
                 } else {
